18feaef740767141efe9f33cd8c91bb384e21540
Luciana <luciana342010@hotmail.com>
2025-01-15 15:40:50 -0300

    Fechando trstes básicos de automação

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get(' .product-block ')
+        // .first()
+        // .last()
+        // .eq(2)
+        .contains('Ajax Full-Zip Sweatshirt')
+        .click()
+        cy.get('#tab-title-description >').should('contain', 'Descrição')
+        
+        
+        
+    });
+    
+});
