5984c74d489b97582a743055ce2f0dd6af03fea9
Luciana <luciana342010@hotmail.com>
2025-01-28 18:47:43 -0300

    Subindo aula page-objet

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,22 +1,39 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
-        
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Argus All-Weather Tank')
         cy.get(' .product-block ')
-        // .first()
-        // .last()
-        // .eq(2)
-        .contains('Ajax Full-Zip Sweatshirt')
-        .click()
+        
         cy.get('#tab-title-description >').should('contain', 'Descrição')
                 
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Helena Hooded Fleece'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain','Helena Hooded Fleece' )
+        
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('helena hooded fleece') 
+        cy.get('.product_title').should('contain','Helena Hooded Fleece' )
+        
+    });
+
+    it.only('Deve adicionar produto ao carrinho', () => {
+        
+    });
+
+
     
     
 });
---
+++ cypress/support/page-objects/produtos.page.js
@@ -0,0 +1,30 @@
+class produtosPage{
+    visitarUrl() {
+        cy.visit('produtos')
+        
+    }
+
+    buscarProduto (nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    buscarProdutoLista (nomeProduto) {
+            cy.get(' .product-block ')
+            .contains(nomeProduto)
+            .click()
+    }
+
+    visitarProduto(nomeProduto){
+        // cy.visit(`produtos/${nomeProduto}`)
+
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+            cy.visit(`produtos/${urlFormatada}`) 
+    }           
+    
+    addProdutoCarrinho () {
+
+    }
+}
+
+export default new produtosPage()
